de8bab80719bb59a814b9db20b37396112b1deec
Chizurum Okere-Olujie <okereolujiechizurum@gmail.com>
2024-08-15 15:50:42 +0100

    Introduction to computer science

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let m = 50;
+
+let n= 10;
+
+let d = m/n
+
+let r = m%n;
+
+function euclideanAlgorithm(m, n, r) {
+   if (r===0) {
+      return n
+   } else {
+      return {m:n, n:r}, d;
+   }
+}
---
+++ script.js
@@ -0,0 +1,26 @@
+ let m = 10;
+let n= 5;
+
+let d = m/n
+let r = m%n;
+
+
+
+
+
+
+function euclideanAlgorithm(m, n, r) {
+   if (r===0) {
+      return n
+   } else {
+      return {m:n, n:r}, d;
+   }
+}
+
+
+
+    
+
+
+
+
